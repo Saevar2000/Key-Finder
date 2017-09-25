0707df868856c7bcbfaaaab6550b35809b9b4392
saevarg.h17 <saevarg.h17@nemandi.ntv.is>
2017-09-25 21:32:41 +0000

    Add project files.

---
+++ Key-Finder/Program.cs
@@ -0,0 +1,28 @@
+﻿using NBitcoin;
+using System;
+
+namespace Key_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf -> 1EHNa6Q4Jz2uvNExL497mE43ikXhwF6kZm
+            string privateKey = "5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf8";
+
+            Validator validator = new Validator();
+            if (validator.ValidatePrivateKey(privateKey)) // Validate the private key first because converting it is much more heavy
+            {
+                Console.WriteLine("Converting private key to a bitcoin address...");
+                BitcoinSecret secret = new BitcoinSecret(privateKey);
+                var bitcoinAddressKey = secret.PrivateKey.PubKey.GetAddress(Network.Main);
+                string bitcoinAddress = bitcoinAddressKey.ToString();
+                Console.WriteLine($"{privateKey} -> {bitcoinAddress}");
+                
+                validator.ValidateBitcoinAddress(bitcoinAddress);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Key-Finder/Validator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace Key_Finder
+{
+    class Validator
+    {
+        public bool ValidatePrivateKey(string privateKey)
+        {
+            if (Regex.IsMatch(privateKey, "5[HJK][1-9A-HJ-NP-Za-km-z]{49}"))
+            {
+                Console.WriteLine("private key is uncompressed");
+                return true;
+            }
+            else if (Regex.IsMatch(privateKey, "[KL][1-9A-HJ-NP-Za-km-z]{51}"))
+            {
+                Console.WriteLine("private key is compressed");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid private key");
+                return false;
+            }
+        }
+
+        public bool ValidateBitcoinAddress(string address)
+        {
+            if (address.Length < 26 || address.Length > 35)
+            {
+                Console.WriteLine("wrong length");
+                return false;
+            }
+            var decoded = DecodeBase58(address);
+            var d1 = Hash(decoded.SubArray(0, 21));
+            var d2 = Hash(d1);
+            if (!decoded.SubArray(21, 4).SequenceEqual(d2.SubArray(0, 4)))
+            {
+                Console.WriteLine("bad digest");
+                return false;
+            }
+            return true;
+        }
+
+        const string Alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+        const int Size = 25;
+
+        private byte[] DecodeBase58(string input)
+        {
+            var output = new byte[Size];
+            foreach (var t in input)
+            {
+                var p = Alphabet.IndexOf(t);
+                if (p == -1) Console.WriteLine("invalid character found");
+                var j = Size;
+                while (--j > 0)
+                {
+                    p += 58 * output[j];
+                    output[j] = (byte)(p % 256);
+                    p /= 256;
+                }
+                if (p != 0) Console.WriteLine("address too long");
+            }
+            return output;
+        }
+
+        private byte[] Hash(byte[] bytes)
+        {
+            var hasher = new SHA256Managed();
+            return hasher.ComputeHash(bytes);
+        }
+    }
+
+    public static class ArrayExtensions
+    {
+        public static T[] SubArray<T>(this T[] data, int index, int length)
+        {
+            var result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+    }
+}
