a35b37dd9acd42d7524a0f52431df395cf47b21a
Saevar2000 <saevar2000@gmail.com>
2017-10-04 21:58:48 +0000

    forgot to add this in the last commit

---
+++ Crawler/Program.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            WriteContents writeContents = new WriteContents();
+            string url = "https://pastebin.com/XQWEefvc/";
+            writeContents.Write(url);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
