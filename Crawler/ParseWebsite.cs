40df0d9044f6686e1044fa8dd70012b7f1d69e0c
Saevar2000 <saevar2000@gmail.com>
2017-10-07 12:36:08 +0000

    forgot to add the file again...

---
+++ Crawler/ParseWebsite.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class ParseWebsite
+    {
+        public void Parse(string url)
+        {
+            try
+            {
+                TextWriter tw = new StreamWriter("test-" + DateTime.Now.Ticks + ".txt ", true);
+                var data = new MyWebClient().DownloadString(url);
+                var doc = new HtmlDocument();
+                doc.LoadHtml(data);
+
+                doc.DocumentNode.Descendants()
+                .Where(n => n.Name == "script" || n.Name == "style" || n.Name == "#comment")
+                .ToList()
+                .ForEach(n => n.Remove());
+
+                foreach (var node in doc.DocumentNode.SelectNodes("//text()"))
+                {
+                    if (!string.IsNullOrWhiteSpace(node.InnerText))
+                    {
+                        tw.WriteLine(node.InnerText);
+                        Console.WriteLine(node.InnerText);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
